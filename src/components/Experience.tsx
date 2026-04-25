5757156602e24d0b2f31660ccbc089681ddf4079
kutukalan <118997325+umutkutukalan@users.noreply.github.com>
2026-04-25 17:54:01 +0300

    feat: comment out room dimensions and mesh elements in Experience component

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -3,35 +3,35 @@ import Item from './Item';
 
 
 export default function Experience({ isNight }: { isNight: boolean }) {
-    const sagduvar = 3; // Odanın derinliği
-    const solduvar = 3; // Odanın genişliği
-    const width = 3; // Kutunun genişliği
+    // const sagduvar = 3;
+    // const solduvar = 3; 
+    // const width = 3; 
 
     return (
         <>
             {/* ZEMİN: Merkezini kaydırarak 0'dan başlatıyoruz */}
-            <mesh rotation={[-Math.PI / 2, 0, 0]} position={[width / 2, 0, sagduvar / 2]} receiveShadow>
+            {/* <mesh rotation={[-Math.PI / 2, 0, 0]} position={[width / 2, 0, sagduvar / 2]} receiveShadow>
                 <planeGeometry args={[width, sagduvar]} />
                 <meshStandardMaterial color="#6e685f" />
-            </mesh>
+            </mesh> */}
 
             {/* ARKA DUVAR: Z ekseninde 0 noktasında sabit */}
-            <mesh position={[width / 2, sagduvar / 2, 0]} receiveShadow>
+            {/* <mesh position={[width / 2, sagduvar / 2, 0]} receiveShadow>
                 <planeGeometry args={[width, sagduvar]} />
                 <meshStandardMaterial transparent opacity={10} />
-            </mesh>
+            </mesh> */}
 
             {/* SOL DUVAR: X ekseninde 0 noktasında sabit */}
-            <mesh rotation={[0, Math.PI / 2, 0]} position={[0, solduvar / 2, solduvar / 2]} receiveShadow>
+            {/* <mesh rotation={[0, Math.PI / 2, 0]} position={[0, solduvar / 2, solduvar / 2]} receiveShadow>
                 <planeGeometry args={[solduvar, solduvar]} />
                 <meshStandardMaterial transparent opacity={10}/>
-            </mesh>
+            </mesh> */}
 
             {/* HALI: Zeminin üzerinde */}
-            <mesh rotation={[-Math.PI / 2, 0, 0]} position={[2.2, 0.02, sagduvar / 2]} receiveShadow>
+            {/* <mesh rotation={[-Math.PI / 2, 0, 0]} position={[2.2, 0.02, sagduvar / 2]} receiveShadow>
                 <planeGeometry args={[width * 0.5, width * 0.5]} />
                 <meshStandardMaterial color="#3b485d" />
-            </mesh>
+            </mesh> */}
 
             {/* MODELLER: Artık x ve z değerlerini 0 ile 4 arasında vererek kutuya dizebilirsin */}
             {roomItems.map((item) => (
